411ef40b7094443c5c84851e471e3de26dd783a8
MudisDev <martin.bibiano4@outlook.com>
2025-09-05 18:51:03 -0600

    Add: TextInputComponents para editar perfil de usuario agregados.
    * Los TextInputComponents muestran los valores del usuario en lugar de un placeholder.
    * Los useState  obtienen los datos del usuario atraves de un useEffect.

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
+import { View, Text, StyleSheet, TextComponent } from 'react-native'
 import { stylesAppTheme } from '../theme/AppTheme'
 import { UserContext } from '../context/UserContext'
 import { useTheme } from '../hooks/UseTheme'
 import { ButtonComponent } from '../components/ButtonComponent'
+import { TextInputComponent } from '../components/TextInputComponent'
 
 
 export const Profile = () => {
@@ -13,6 +14,24 @@ export const Profile = () => {
   const { userData } = useContext(UserContext) || { setUserData: () => { } }; // Maneja el caso de que el contexto no esté definido
 
   const [editarPerfil, setEditarPerfil] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [gender, setGender] = useState<string>("");
+
+  useEffect(() => {
+
+    if (editarPerfil) {
+      setName(userData?.name || "");
+      setUsername(userData?.username || "");
+      setEmail(userData?.email || "");
+      setGender(userData?.gender || "");
+    }
+
+  }, [editarPerfil])
+
+
 
 
   const noFunction = () => { }
@@ -46,10 +65,20 @@ export const Profile = () => {
           </View>
           <Text></Text>
         </> :
-        <></>
+        <>
+          <TextInputComponent placeholderText='Nombre' action={setName} value={name} isPassword={false} verified={false} />
+          <Text></Text>
+          <TextInputComponent placeholderText='Usuario' action={setUsername} value={username} isPassword={false} verified={false} />
+          <Text></Text>
+          <TextInputComponent placeholderText='Correo E.' action={setEmail} value={email} isPassword={false} verified={false} />
+          <Text></Text>
+          <TextInputComponent placeholderText='Genero' action={setGender} value={gender} isPassword={false} verified={false} />
+          <Text></Text>
+
+        </>
       }
 
-      <ButtonComponent title={editarPerfil ? 'Guardar cambios' : 'Editar perfil'} funcion={() => setEditarPerfil(!editarPerfil) } active={true} />
+      <ButtonComponent title={editarPerfil ? 'Guardar cambios' : 'Editar perfil'} funcion={() => setEditarPerfil(!editarPerfil)} active={true} />
 
       <Text></Text>
       {/*
