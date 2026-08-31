4e0ab282e6e5fdccb6fa59b8d372c212f1d18aef
MudisDev <martin.bibiano4@outlook.com>
2026-08-31 15:17:52 -0600

    Add: useFetch.js agregado.
    * Se pasó tal cual el useFetch.js de WaifuPaper Control Panel.

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+export const useFetch = ({ endpoint, metodo, params = {} }) => {
+  const [data, setData] = React.useState([]);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  const metodosPermitidos = ["GET", "POST", "PUT", "PATCH", "DELETE"];
+
+  const fetchData = async (newParams = params) => {
+    if (!metodosPermitidos.includes(metodo)) {
+      console.error({ Error: "Metodo en useFetch invalido" });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setError(null);
+
+      const queryParams = newParams;
+
+      let url = endpoint;
+      let opciones = { method: metodo, credentials: "include" };
+
+      if (metodo === "GET") {
+        const query = new URLSearchParams(queryParams);
+
+        if (query.toString()) {
+          url = `${endpoint}?${query.toString()}`;
+        }
+      } else {
+        opciones.headers = {
+          "Content-Type": "application/json",
+        };
+
+        opciones.body = JSON.stringify(queryParams);
+      }
+
+      const response = await fetch(url, opciones);
+
+      // 👇 Primero obtenemos la respuesta como texto
+      const texto = await response.text();
+
+      console.log("Respuesta RAW del servidor:", texto);
+
+      // 👇 Intentamos convertir ese texto a JSON
+      let json;
+
+      try {
+        json = JSON.parse(texto);
+      } catch (e) {
+        throw new Error(
+          `El servidor no devolvió JSON válido: ${texto}, ERROR CACHADO -> ${e}`,
+        );
+      }
+
+      if (!response.ok) {
+        const errorData = {
+          status: response.status,
+          ...json,
+        };
+
+        setError(errorData);
+        return errorData;
+      }
+
+      setData(json);
+      return json;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, error, loading, fetchData };
+};
